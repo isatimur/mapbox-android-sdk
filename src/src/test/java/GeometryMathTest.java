91980d5471a571486ac703abbd950706cf013d2e
Tom MacWright <tom@macwright.org>
2014-02-06 16:29:59 -0500

    Stash valiant attempt at making tests a thing.

---
+++ src/src/test/java/GeometryMathTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class GeometryMathTest {
+
+    @Test
+    public void shouldFail() {
+        assertTrue(false);
+    }
+}
